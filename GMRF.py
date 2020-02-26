fd0bac278b4b7e3ec79f3352274c1e1e4cf57e6f
Xue Li <xli26@nd.edu>
2020-02-26 16:52:14 -0500

    Code review for GMRF modeling.

--- GMRF.py
+++ GMRF.py
@@ -4,15 +4,35 @@ from vtk.util.numpy_support import numpy_to_vtk
 from sksparse.cholmod import cholesky
 from scipy.sparse import csc_matrix, diags, issparse, linalg as sla
 from scipy.special import gamma
+from scipy.special import kv
 from math import pi, sqrt
 import numpy as np
 import vtk
 import timeit
 import matplotlib.pyplot as plt
-from scipy.special import kv
+import os.path
+
 
 gdim = 2.0
 gnu = 2.0
+
+def Dijkstra(nNodes, nodes, neighbors, nbdists, source=0):
+    Q = list(range(nNodes))
+    dist = np.full(nNodes, np.inf)
+    dist[source] = 0.0
+
+    while len(Q) > 0:
+        u = Q[np.argmin(dist[Q])]
+        Q.remove(u)
+        for iv, v in enumerate(neighbors[u]):
+            if v not in Q:
+                continue
+            alt = dist[u] + nbdists[u][iv]
+            if alt < dist[v]:
+                dist[v] = alt
+
+    return dist
+
 
 def matern_covariance(d, nu=1.0, k=1.0):
     var = gamma(nu) / (gamma(nu+gdim/2.0) * ((4*pi)**(gdim/2.0)) * k**(2*nu))
@@ -22,11 +42,12 @@ def matern_covariance(d, nu=1.0, k=1.0):
     # print cov
     return cov
 
-def check_correlation(X, npNodes, k):
+def check_correlation(X, npNodes, k, dists, source=0):
     ptsidx = np.random.choice(np.arange(1, len(npNodes)), 100)
     corX = np.corrcoef(X)
-    distance = np.sqrt(np.sum((npNodes[ptsidx] - npNodes[0]) ** 2, axis=1))
-    plt.plot(distance, corX[0, ptsidx], 'bo', markersize=3.0, label='generation')
+    # cov = np.cov(X)
+    distance = dists[ptsidx]
+    plt.plot(distance, corX[source, ptsidx], 'bo', markersize=3.0, label='generation')
     plt.plot(distance, matern_covariance(distance, nu=gnu, k=k), 'ro', markersize=3.0, label='Matern') # nu=0.5 for 3-dim, 1.0 for 2-dim
     plt.ylabel('Correlation',fontsize=17)
     plt.xlabel('Distance',fontsize=17)
@@ -45,19 +66,84 @@ def check_variance(X, dim, nu, k):
     plt.legend()
     plt.show()
 
+def unique(xlist):
+    unique_list = []
+    for x in xlist:
+        if x not in unique_list:
+            unique_list.append(x)
+    return unique_list
+
+def check(filename, geofilename, rho, nu=2.0):
+
+    # Read triangulation from file.
+    print('Reading File...')
+    reader = vtk.vtkXMLPolyDataReader()
+    reader.SetFileName(geofilename)
+    reader.Update()
+    polyDataModel = reader.GetOutput()
+
+    totalNodes = polyDataModel.GetNumberOfPoints()
+    vtkNodes = polyDataModel.GetPoints().GetData()
+    npNodes = vtk_to_numpy(vtkNodes)
+
+    nElements = polyDataModel.GetNumberOfCells()
+    elements = np.empty((nElements, 3), dtype=int)
+    for iElm in range(nElements):
+        vtkCell = polyDataModel.GetCell(iElm)
+        for ipt in range(3):
+            elements[iElm,ipt] = vtkCell.GetPointId(ipt)
+
+    # Collect the neighbors information.
+    neighbors = [[] for _ in range(totalNodes)]
+    uniqueNbs = []
+    nbdists = []
+    for iElm in range(nElements):
+        for iNode in elements[iElm]:
+            neighbors[iNode].extend(elements[iElm])
+    for iNode in range(totalNodes):
+        uniqueNbs.append(unique(neighbors[iNode]).remove(iNode))
+        nbdists.append(np.linalg.norm(npNodes[neighbors[iNode]]-npNodes[iNode], axis=1))
+
+    # Prepare the distance information.
+    dists = Dijkstra(totalNodes, npNodes, neighbors, nbdists)
+
+    # Read the random field generated.
+    X = np.load(filename)
+
+    # Calculate kappa
+    kappa = ((2.0*nu)**0.5)/rho
+
+    print('Ploting...')
+    check_correlation(X, npNodes, kappa, dists)
+    # check_variance(X, 2.0, nu, kappa)
+
+
+def readNoise(filename, samplenum):
+    print('Generating samples...')
+    if os.path.exists(filename):
+        return np.load(filename)
+
+    # Z = np.random.normal(size=(totalNodes, samplenum))
+    # Z = np.empty((totalNodes, samplenum))
+    # # for i in range(samplenum):
+    # #     Z[:,i] = np.random.normal(size=totalNodes)
+    # for i in range(totalNodes):
+    #     Z[i,:] = np.random.normal(size=samplenum)
+    Z = np.random.multivariate_normal(np.zeros(totalNodes), np.identity(totalNodes), samplenum).T
+    np.save(filename, Z)
+    return Z
+
+
 def loc(indptr, indices, i, j):
     return indptr[i] + np.where(indices[indptr[i]:indptr[i+1]]==j)[0]
 
-
 class GMRF:
 
-    def __init__(self, filename, dim=2.0, nu=2.0, rho=0.95):
-
-        kappa = ((8.0*nu)**0.5)/rho
+    def __init__(self, filename, dim=2.0, nu=2.0):
 
         # start_time = timeit.default_timer()
         # Read triangulation from file.
-        # print 'Reading File...'
+        print('Reading File...')
         reader = vtk.vtkXMLPolyDataReader()
         reader.SetFileName(filename)
         reader.Update()
@@ -66,13 +152,10 @@ class GMRF:
         totalNodes = polyDataModel.GetNumberOfPoints()
         vtkNodes = polyDataModel.GetPoints().GetData()
         npNodes = vtk_to_numpy(vtkNodes)
-        # print(vtkPointData)
-        # print(npNodes)
 
-        # print 'Total nodes: ', totalNodes
         # print timeit.default_timer() - start_time
         # start_time = timeit.default_timer()
-        # print 'Building Topology...'
+        print('Building Topology...')
         totalElms = polyDataModel.GetNumberOfCells()
         # Get cells from source file.
         npElms = np.zeros((totalElms, 3), dtype=int)
@@ -93,8 +176,8 @@ class GMRF:
         # print timeit.default_timer() - start_time
         # start_time = timeit.default_timer()
 
-        # print 'Creating the sparse matrix...'
         # Create sparse data structure.
+        print('Creating the sparse matrix...')
         sparseInfo = [[] for _ in range(totalNodes)]
         for icell in range(totalElms):
             for inode in npElms[icell]:
@@ -103,30 +186,28 @@ class GMRF:
         sparseInfo = np.array(sparseInfo)
         for knodes in range(totalNodes):
             sparseInfo[knodes] = np.unique(sparseInfo[knodes])
-        # print(sparseInfo)
 
         # Generate the sparse matrix.
         indptr = [0]
         indices = []
         for inode in range(totalNodes):
             indices.extend(sparseInfo[inode])
-            indptr.extend([len(indices)])
+            indptr.append(len(indices))
         rawC = np.zeros(len(indices))
         rawG = np.zeros(len(indices))
-        # rawCTuta = np.zeros(totalNodes)
 
         # print timeit.default_timer() - start_time
         # start_time = timeit.default_timer()
 
         # print 'Assembling global matrix...'
         # Generate C and G matrix.
-        cm = np.array([[2.0, 1.0, 1.0], [1.0, 2.0, 1.0], [1.0, 1.0, 2.0]])
+        cm = np.array([[2.0, 1.0, 1.0], [1.0, 2.0, 1.0], [1.0, 1.0, 2.0]]) / 12.0
         # dcm = np.array([1.0, 1.0, 1.0])
         for icell in range(totalElms):
             # Compute local matrix first.
-            localc = cm * npAreas[icell] / 12.0
+            localc = cm * npAreas[icell]
             # localdc = dcm * npAreas[icell] / 3.0
-            localg = np.dot(npEdges[icell], npEdges[icell].transpose()) / (4 * npAreas[icell])
+            localg = np.dot(npEdges[icell], npEdges[icell].transpose()) / (4.0 * npAreas[icell])
             # Assembly to the glabal matrix.
             for i in range(3):
                 # rawCTuta[npElms[icell, i]] += localdc[i]
@@ -149,12 +230,31 @@ class GMRF:
         # invC = factorC.inv()
 
         # print timeit.default_timer() - start_time
+        # Remember things need to remember.
+        self.dim = dim
+        self.nu = nu
+        self.C = C
+        self.G = G
+        self.invCTuta = invCTuta
+
+        self.polyDataModel = polyDataModel
+        self.totalNodes = totalNodes
+        self.npNodes = npNodes
+
+
+    def setRho(self, rho=0.95):
+
+        C = self.C
+        G = self.G
+        invCTuta = self.invCTuta
+        nu = self.nu
+
+        kappa = ((2.0*nu)**0.5)/rho
         # start_time = timeit.default_timer()
 
         # Compute Q matrix according to C and G.
         # print 'Computing K...'
         K = (kappa**2)*C + G
-        # print K.todense()
 
         # print timeit.default_timer() - start_time
         # start_time = timeit.default_timer()
@@ -162,8 +262,8 @@ class GMRF:
         # print 'Computing of Q...'
         Q1 = K
         Q2 = (K.dot(invCTuta)).dot(K) # Q2
-        Q = (((K.dot(invCTuta)).dot(Q1)).dot(invCTuta)).dot(K)
-        # Q = (((K.dot(invCTuta)).dot(Q2)).dot(invCTuta)).dot(K)
+        Q = (((K.dot(invCTuta)).dot(Q1)).dot(invCTuta)).dot(K) # Q3
+        # Q = (((K.dot(invCTuta)).dot(Q2)).dot(invCTuta)).dot(K) # Q4
 
         # alpha = int(nu+dim/2.0)
         # if alpha % 2 == 1:
@@ -196,17 +296,10 @@ class GMRF:
         PT[P] = np.arange(len(P))
 
         # print timeit.default_timer() - start_time
-
-        # Remember things need to remember.
-        self.dim = dim
-        self.nu = nu
         self.kappa = kappa
         self.factorQ = factorQ
         self.PT = PT
 
-        self.polyDataModel = polyDataModel
-        self.totalNodes = totalNodes
-        self.npNodes = npNodes
 
     def generate(self, mu, sigma, Z, resfilename=None, lb=None):
 
@@ -216,24 +309,27 @@ class GMRF:
         # print 'Solving upper triangular syms...'
         X = self.factorQ.solve_Lt(Z, use_LDLt_decomposition=False)
         X = X[self.PT]
-        # print np.allclose(, Z)
 
-        # sigmaReal0 = np.std(X)
-        # sigmaReal1 = np.amax(np.std(X, axis=0))
-        # sigmaReal2 = np.amax(np.std(X, axis=1))
-        # sigmaReal3 = np.mean(np.std(X, axis=1))
-        # print(sigmaReal0, sigmaReal1, sigmaReal2, sigmaReal3)
-        # return
-        # sigmaReal = min(np.amax(np.std(X, axis=0)), np.amax(np.std(X, axis=1)))
-        # sigmaReal = (np.std(X) + np.amax(np.std(X, axis=1))) / 2.0
-        sigmaReal = np.std(X) + (np.amax(np.std(X, axis=1)) - np.std(X)) * 0.667
+        # # sigmaReal0 = np.std(X)
+        # # sigmaReal1 = np.amax(np.std(X, axis=0))
+        # # sigmaReal2 = np.amax(np.std(X, axis=1))
+        # # sigmaReal3 = np.mean(np.std(X, axis=1))
+        # # print(sigmaReal0, sigmaReal1, sigmaReal2, sigmaReal3)
+        # # return
+        # # sigmaReal = min(np.amax(np.std(X, axis=0)), np.amax(np.std(X, axis=1)))
+        # # sigmaReal = (np.std(X) + np.amax(np.std(X, axis=1))) / 2.0
+        # sigmaReal = np.std(X) + (np.amax(np.std(X, axis=1)) - np.std(X)) * 0.667
+        # sigmaRatio = sigma/sigmaReal
+        # X = X*sigmaRatio + mu
+        # # print timeit.default_timer() - start_time
+
+        sigmaReal = np.std(X, axis=1)
         sigmaRatio = sigma/sigmaReal
-        X = X*sigmaRatio + mu
-        # print timeit.default_timer() - start_time
+        X = X*sigmaRatio[:,np.newaxis] + mu
 
-        # X[X<=0.0] = 0.01
-        if lb is not None:
-            X[X<lb] = lb
+        # # X[X<=0.0] = 0.01
+        # if lb is not None:
+        #     X[X<lb] = lb
 
         if resfilename is not None:
             # start_time = timeit.default_timer()
@@ -255,23 +351,21 @@ class GMRF:
 
             # print timeit.default_timer() - start_time
 
-        print('Ploting...')
-        check_correlation(X, self.npNodes, self.kappa)
-        # check_variance(X, self.dim, self.nu, self.kappa)
-        # check_variance(X, 3, 0.5, kappa)
-
         return X
 
 
-if __name__ == '__main__':
+def generateGFs():
 
     solidfile = 'Examples/CylinderProject/refine-mesh-complete/walls_combined.vtp'
+    noisefile = 'Examples/CylinderProject/RefineWallProperties/noise.npy'
     totalNodes = 7628
-    resThickness = 'Examples/CylinderProject/RefineWallProperties/thickness'
+    # resThickness = 'Examples/CylinderProject/RefineWallProperties/thickness'
+    resThickness = 'cyThickness'
     thick_mu = 0.4
     thick_sigma = 0.04
     thick_lb = 0.28
-    resE = 'Examples/CylinderProject/RefineWallProperties/YoungsModulus'
+    # resE = 'Examples/CylinderProject/RefineWallProperties/YoungsModulus'
+    resE = 'cyYoungsModulus'
     E_mu = 7.0e6
     E_sigma = 7.0e5
 
@@ -288,19 +382,32 @@ if __name__ == '__main__':
 
 
     samplenum = 100
-    # print 'Generating samples...'
     # Generate normal distrib random nums & combine.
-    # Z = np.random.normal(size=(totalNodes, samplenum))
-    Z = np.empty((totalNodes, samplenum))
-    # for i in range(samplenum):
-    #     Z[:,i] = np.random.normal(size=totalNodes)
-    for i in range(totalNodes):
-        Z[i,:] = np.random.normal(size=samplenum)
+    # Z = readNoise(noisefile, samplenum)
+    np.random.seed(23)
+    Z = np.random.normal(size=(totalNodes, samplenum))
+
+    rhos = np.array([0.95, 3.7, 7.2])
+    gf = GMRF(solidfile)
+    for rho in rhos:
+        gf.setRho(rho)
+        gf.generate(mu=thick_mu, sigma=thick_sigma, Z=Z, resfilename='{}{}'.format(resThickness, rho), lb=thick_lb)
+        gf.generate(mu=E_mu, sigma=E_sigma, Z=Z, resfilename='{}{}'.format(resE, rho))
+
+
+def checkGFs():
+
+    solidfile = 'Examples/CylinderProject/refine-mesh-complete/walls_combined.vtp'
+    resThickness = 'cyThickness'
+    resE = 'cyYoungsModulus'
 
     rhos = np.array([0.95, 3.7, 7.2])
     for rho in rhos:
-        gf = GMRF(solidfile, rho=rho)
+        check('{}{}.npy'.format(resThickness, rho), solidfile, rho)
 
-        gf.generate(mu=thick_mu, sigma=thick_sigma, Z=Z, resfilename='{}{}'.format(resThickness, rho), lb=thick_lb)
-        gf.generate(mu=E_mu, sigma=E_sigma, Z=Z, resfilename='{}{}'.format(resE, rho))
+
+if __name__ == '__main__':
+
+    # generateGFs()
+    checkGFs()
 
