74d2442c9b80e54db24f5656e691eb2009ca62c1
melindalx <xli26@nd.edu>
2021-04-09 18:45:46 -0400

    Debugging explicit VMS, get rid of mass lumping.

--- explicitVMSSolver.py
+++ explicitVMSSolver.py
@@ -8,7 +8,10 @@
 """
 
 import numpy as np
+from scipy.sparse import csc_matrix
+from scipy.sparse.linalg import gmres, LinearOperator, spilu
 from mpi4py import MPI
+
 from physicsSolver import *
 from optimizedExplicitVMSAssemble import OptimizedExplicitVMSAssemble
 from optimizedExplicitVMSAssemble import OptimizedExplicitVMSInitialAssemble
@@ -17,6 +20,10 @@ from optimizedExplicitVMSAssemble import OptimizedExplicitVMSInitialAssemble
 # Parameters for the explicit solver.
 c1 = 4.0
 c2 = 2.0
+
+vDof = 3
+pDof = 1
+dof = 4
 
 
 class ExplicitVMSSolver(PhysicsSolver):
@@ -32,9 +39,9 @@ class ExplicitVMSSolver(PhysicsSolver):
         self.du = mesh.iniDu.reshape((self.mesh.nNodes, 3)) # velocity
         self.p = mesh.iniP # pressure
 
-        # self.odu = np.zeros_like(self.du)
-        # self.op = np.zeros_like(self.p)
-        
+        self.odu = np.zeros_like(self.du)
+        self.op = np.zeros_like(self.p)
+
         self.sdu = np.zeros((self.mesh.nElements, 4, 3)) # sub-scale velocity
         self.nsdu = np.zeros_like(self.sdu) # sdu at next time step
         self.sp = np.zeros((self.mesh.nElements, 4))
@@ -48,12 +55,10 @@ class ExplicitVMSSolver(PhysicsSolver):
         self.f = self.mesh.f * np.ones((self.mesh.nNodes, 3))
         # Initialize shape functions ...
         self.InitializeParameters()
+        self.InitializeSolver()
         # Initialize the boundary conditions
         self.ApplyDirichletBCs(0.0)
-        # Initialize solver
-        # self.InitializeSolver()
-        self.odu = np.zeros_like(self.du)
-        self.op = np.zeros_like(self.p)
+
 
     def InitializeParameters(self):
         # Parameters for Tetrahedron
@@ -63,7 +68,7 @@ class ExplicitVMSSolver(PhysicsSolver):
         self.lN = np.array([[alpha, beta, beta, beta],
                             [beta, alpha, beta, beta],
                             [beta, beta, alpha, beta],
-                            [beta, beta, alpha, beta]])
+                            [beta, beta, beta, alpha]])
         self.lDN = np.array([[-1.0, 1.0, 0.0, 0.0],
                              [-1.0, 0.0, 1.0, 0.0],
                              [-1.0, 0.0, 0.0, 1.0]])
@@ -71,37 +76,57 @@ class ExplicitVMSSolver(PhysicsSolver):
         self.coefs = np.array([c1, c2, self.mesh.dviscosity, self.dt, 1.0])
 
     def InitializeSolver(self):
-        # Calculate the dt, dp
-        self.LHS = np.zeros(self.mesh.nNodes*self.Dof)
-        self.Res = np.zeros(self.mesh.nNodes*self.Dof)
+        
+        nodes = self.mesh.nodes
+        elements = self.mesh.elementNodeIds
 
-        OptimizedExplicitVMSInitialAssemble(self.mesh.nodes, self.mesh.elementNodeIds,
-                                            self.du, self.p, self.f,
-                                            self.lN, self.lDN, self.w, self.coefs,
-                                            self.LHS, self.Res)
-        acc = np.divide(-self.Res, self.LHS, out=np.zeros_like(self.Res), where=self.LHS!=0)
-        acc = acc.reshape((self.mesh.nNodes, self.Dof))
-        self.odu = self.du - self.dt*acc[:,:3]
-        self.op = self.p - self.dt*acc[:,-1]
+        nNodes = nodes.shape[0]
+        nElms = elements.shape[0]
+        nElmNodes = elements.shape[1]
+
+        w = self.w
+        lN = self.lN
+        lDN = self.lDN
+
+        # --- Initial assemble: DNs and LHS
+        self.DNs = np.empty((nElms, vDof, nElmNodes), dtype=float)
+        self.volumes = np.zeros(nElms, dtype=float) # For debugging
+        self.LHS = np.zeros((dof*nNodes, dof*nNodes), dtype=float)
+
+        OptimizedExplicitVMSInitialAssemble(nodes, elements, w, lN, lDN,
+                                            self.DNs, self.volumes, self.LHS)
+
+        # Sparse matrix
+        self.spLHS = csc_matrix(self.LHS)
+        myPreconditioner = spilu(self.spLHS)
+        M_x = lambda x: myPreconditioner.solve(x)
+        self.M = LinearOperator((dof*nNodes, dof*nNodes), M_x)
+
+        # --- Attach the initial velocity and pressure together
+        self.res = np.empty((nNodes, dof), dtype=float)
+        self.res[:,:3] = self.du
+        self.res[:,-1] = self.p
 
 
     def Solve(self, t, dt):
 
-        self.LHS = np.zeros(self.mesh.nNodes*self.Dof)
-        self.RHS = np.zeros(self.mesh.nNodes*self.Dof)
-        self.Res = np.zeros(self.mesh.nNodes*self.Dof)
+        nodes = self.mesh.nodes
+        elements = self.mesh.elementNodeIds
+
+        nNodes = nodes.shape[0]
+
+        self.RHS = np.zeros(nNodes*dof)
+        self.R = np.zeros(nNodes*dof) # residual
 
         # only for debugging
-        self.mRT1 = np.zeros(self.mesh.nNodes*3)
-        self.mRT2 = np.zeros(self.mesh.nNodes*3)
-        self.mRT3 = np.zeros(self.mesh.nNodes*3)
-        self.mRT4 = np.zeros(self.mesh.nNodes*3)
-        self.mRT5 = np.zeros(self.mesh.nNodes*3)
-        self.mRT6 = np.zeros(self.mesh.nNodes*3)
-        self.mRT7 = np.zeros(self.mesh.nNodes*3)
+        self.mRT1 = np.zeros(nNodes*vDof)
+        self.mRT2 = np.zeros(nNodes*vDof)
+        self.mRT3 = np.zeros(nNodes*vDof)
+        self.mRT4 = np.zeros(nNodes*vDof)
+        self.mRT5 = np.zeros(nNodes*vDof)
 
-        self.pRT1 = np.zeros(self.mesh.nNodes)
-        self.pRT2 = np.zeros(self.mesh.nNodes)
+        self.pRT1 = np.zeros(nNodes)
+        self.pRT2 = np.zeros(nNodes)
 
 
         # Evaluate velocity prediction hdu and pressure prediction hp
@@ -111,53 +136,50 @@ class ExplicitVMSSolver(PhysicsSolver):
 
         # Calculate the invEpsilon for artificial incompressible coef.
         ASS = 5.0
-        # self.coefs[4] = (ASS*np.amax(np.linalg.norm(self.du, axis=1)))**2
-        # self.coefs[4] = 34521.64
-        # self.coefs[4] = 3025.0
         self.coefs[4] = (5.0*11.7)**2.0
+        # self.coefs[4] = (5.0*11.7*11.7)**2.0
         print('The invEpsilon = {}'.format(self.coefs[4]))
 
         # # Assemble the LHS and RHS.
-        # OptimizedExplicitVMSAssemble(self.mesh.nodes, self.mesh.elementNodeIds,
+        # OptimizedExplicitVMSAssemble(nodes, elements,
         #                              self.du, self.p, hdu, hp, self.sdu, self.nsdu,
         #                              self.f, self.mesh.inscribeDiameters,
-        #                              self.lN, self.lDN, self.w, self.coefs,
-        #                              self.LHS, self.RHS, self.Res)
+        #                              self.w, self.lN, self.lDN, self.coefs,
+        #                              self.RHS, self.R)
 
         # only for debugging
-        OptimizedExplicitVMSAssemble(self.mesh.nodes, self.mesh.elementNodeIds,
-                                     self.du, self.p, hdu, hp, self.sdu, self.nsdu, self.sp, self.nsp, 
+        OptimizedExplicitVMSAssemble(nodes, elements, self.du, self.p, hdu, hp, self.sdu, self.nsdu,
                                      self.f, self.mesh.inscribeDiameters,
-                                     self.lN, self.lDN, self.w, self.coefs,
-                                     self.LHS, self.RHS, self.Res, self.mRT1, self.mRT2,
-                                     self.mRT3, self.mRT4, self.mRT5, self.mRT6, self.mRT7,
+                                     self.w, self.lN, self.DNs, self.volumes, self.coefs,
+                                     self.RHS, self.R,
+                                     self.mRT1, self.mRT2, self.mRT3, self.mRT4, self.mRT5,
                                      self.pRT1, self.pRT2)
 
-
         # Solve
-        self.odu = self.du
-        self.op = self.p
+        self.odu[:,:] = self.du
+        self.op[:] = self.p
 
         self.sdu = self.nsdu
         self.nsdu = np.zeros_like(self.sdu)
-        self.sp = self.nsp
-        self.nsp = np.zeros_like(self.sp)
 
-        # res = self.RHS / self.LHS
-        res = np.divide(self.RHS-dt*self.Res, self.LHS, out=np.zeros_like(self.RHS), where=self.LHS!=0)
-        res = res.reshape((self.mesh.nNodes, self.Dof))
-        self.du = res[:,:3].ravel().reshape((self.mesh.nNodes, 3))
-        self.p = res[:,-1].ravel()
+        self.res, exitCode = gmres(self.spLHS, self.RHS-dt*self.R, x0=self.res.ravel(), M=self.M)
+        print('Linear system solver at time step {}, converge {}'.format(t, exitCode))
+        print(np.allclose(self.spLHS.dot(self.res), self.RHS-dt*self.R))
+
+        self.res = self.res.reshape((nNodes, dof))
+        self.du[:,:] = self.res[:,:3]
+        self.p[:] = self.res[:,-1]
 
         # Apply the Dirichlet boundary conditions.
         self.ApplyDirichletBCs(t)
+        print('Executing here!')
+
 
     def ApplyDirichletBCs(self, t):
         # Update the inlet velocity first.
         self.mesh.updateInletVelocity(t)
         # Combine the boundary condition at the start of each time step.
         for inlet in self.mesh.faces['inlet']:
-            # dofs = self.GenerateDofs(inlet.appNodes, 3)
             self.du[inlet.appNodes] = inlet.inletVelocity.reshape((len(inlet.appNodes), 3))
 
         # dofs = self.GenerateDofs(self.mesh.wall, 3)
@@ -166,26 +188,22 @@ class ExplicitVMSSolver(PhysicsSolver):
         # Only for debugging
         self.p[self.mesh.outlet] = 0.0
 
-    # def GenerateDofs(self, nodes, dof):
-    #     baseArray = np.arange(dof)
-    #     return np.array([node*dof+baseArray for node in nodes])
 
     def Save(self, filename, counter):
         # self.mesh.Save(filename, counter, self.du.reshape(self.mesh.nNodes, 3), self.p, 'velocity')
 
-        res = self.Res.reshape((self.mesh.nNodes, self.Dof))
+        res = self.R.reshape((self.mesh.nNodes, self.Dof))
         resDu = res[:,:3].ravel()
         resP = res[:,-1].ravel()
 
         vals = [self.du.reshape(self.mesh.nNodes, 3), self.p, resDu.reshape(self.mesh.nNodes, 3), resP,
                 self.mRT1.reshape(self.mesh.nNodes, 3), self.mRT2.reshape(self.mesh.nNodes, 3),
                 self.mRT3.reshape(self.mesh.nNodes, 3), self.mRT4.reshape(self.mesh.nNodes, 3),
-                self.mRT5.reshape(self.mesh.nNodes, 3), self.mRT6.reshape(self.mesh.nNodes, 3),
-                self.mRT7.reshape(self.mesh.nNodes, 3), self.pRT1, self.pRT2]
+                self.mRT5.reshape(self.mesh.nNodes, 3), self.pRT1, self.pRT2]
         names = ['velocity', 'pressure', 'momentum_res', 'pressure_res', 'momentum_res_term1',
                  'momentum_res_term2', 'momentum_res_term3', 'momentum_res_term4', 'momentum_res_term5',
-                 'momentum_res_term6', 'momentum_res_term7', 'pressure_res_term1', 'pressure_res_term2']
-        ptData = np.ones(13, dtype=bool)
+                 'pressure_res_term1', 'pressure_res_term2']
+        ptData = np.ones(11, dtype=bool)
         
         self.mesh.DebugSave(filename, counter, vals, names, ptData)
 
